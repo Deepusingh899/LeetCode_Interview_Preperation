9d0de45143d2e8eaa9dca5e83450243bd9b90b28
Deepusingh899 <deepusingh8527@gmail.com>
2024-12-21 12:41:46 +0530

    21-12-2024 Daily Challages

---
+++ Dec_2024/Maximum_Number_of_K-Divisible_Components.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxKDivisibleComponents(int n, int[][] edges, int[] values, int k) {
+        List<Integer>[] adjList=new ArrayList[n];
+        for(int i=0;i<n;i++){
+            adjList[i]=new ArrayList<>();
+        }
+        for(int []edge:edges){
+            int node1=edge[0];
+            int node2=edge[1];
+            adjList[node1].add(node2);
+            adjList[node2].add(node1);
+        }
+        int[] countComponents=new int[1];
+        dfs(0,-1,adjList,values,k,countComponents);
+        return countComponents[0];
+    }
+    public long dfs(int currentNode,int parent,List<Integer>[] adjList,int[] values,int k,int[] countComponents){
+        long sum=0;
+        for(int neighbours: adjList[currentNode]){
+            if(neighbours!=parent){
+                sum+=dfs(neighbours,currentNode,adjList,values,k,countComponents);
+            }
+        }
+        sum+=values[currentNode];
+        if(sum%k==0){
+            countComponents[0]++;
+            sum=0;
+        }
+        return sum;
+    }
+}
